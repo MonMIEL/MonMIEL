4b5f8846de1c32182a9967e41622170e2bc8acf5
Paul Valla <paul.valla@gmail.com>
2013-03-04 15:29:35 +0100

    added api bundle, and routing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,10 +23,10 @@ class AppKernel extends Kernel
             new Monmiel\MonmielApiModelBundle\MonmielMonmielApiModelBundle(),
             new Monmiel\MonmielDaoBundle\MonmielMonmielDaoBundle(),
             new Monmiel\MixRepartitionBundle\MonmielMixRepartitionBundle(),
+            new Monmiel\MonmielApiBundle\MonmielMonmielApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
