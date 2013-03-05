fbdbabeba762bcc823757d100379452fc4d4bc30
b-q <bingzhi.qiao@gmail.com>
2013-03-05 08:44:05 +0100

    Method add for transformation

--- src/Monmiel/MonmielApiBundle/TransformersService/TransformersV1.php
+++ src/Monmiel/MonmielApiBundle/TransformersService/TransformersV1.php
@@ -7,6 +7,7 @@ namespace Monmiel\MonmielApiBundle\TransformersService;
  */
 class TransformersV1
 {
+
     public function test() {
         $this->daoService->getDayConso("2");
     }
@@ -16,4 +17,49 @@ class TransformersV1
      * @var \Monmiel\MonmielApiBundle\Dao\RiakDao
      */
     public $daoService;
+
+
+    /**
+     * <code>
+     * @include \Monmiel\MonmielApiModelBundle\Model\Quarter.php
+     * </code>
+     *
+     * @param $listQuarter  list des quarters à traiter
+     * @param $consoAct     la consommation actuelle
+     * @param $consoUser    la consommation saisie par utilisateur
+     *
+     * @return array<\Monmiel\MonmielApiModelBundle\Model\Quarter>
+     */
+
+    public function transformeTotalToConsoTher(array $listQuarter,$consoAct,$consoUser){
+
+
+
+        foreach ($listQuarter as $value) {
+
+
+
+
+        }
+
+
+    }
+
+
+    /**
+     * @param $listQuarter
+     * @return array<\Monmiel\MonmielApiModelBundle\Model\Jour_DAO>
+     */
+    public function transformeLQuarterToLJourDAO($listQuarter){
+
+
+    }
+
+
+
+
+
+
+
+
 }
