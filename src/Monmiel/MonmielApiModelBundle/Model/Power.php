19b5c6c7156283c612a381a22fd1e13c25aeb37c
b-q <bingzhi.qiao@gmail.com>
2013-03-07 15:36:28 +0100

    Les puissances

---
+++ src/Monmiel/MonmielApiModelBundle/Model/Power.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: qiaob
+ * Date: 07/03/13
+ * Time: 15:21
+ * To change this template use File | Settings | File Templates.
+ */
+class Power
+{
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $nuclear;
+
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $flame;
+
+
+
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $wind;
+
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $hydraulic;
+
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $photovoltaic;
+
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $other;
+
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $import;
+
+    /**
+     * @var float
+     * @Ser\Type("float")
+     */
+    protected $step;
+}
