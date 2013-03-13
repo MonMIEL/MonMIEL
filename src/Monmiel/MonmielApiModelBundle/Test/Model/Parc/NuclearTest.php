ffacaae9558124c15d7f2692ec81556604423528
patrice <patrice.randriah@gmail.com>
2013-03-13 11:44:34 +0100

    add unit test for nuclear park

--- src/Monmiel/MonmielApiModelBundle/Test/Model/Parc/NuclearTest.php
+++ src/Monmiel/MonmielApiModelBundle/Test/Model/Parc/NuclearTest.php
@@ -9,7 +9,7 @@ class NuclearTest extends \PHPUnit_Framework_TestCase
     /**
      * @var Nuclear
      */
-    protected $object;
+    protected $nuclear;
 
     /**
      * Sets up the fixture, for example, opens a network connection.
@@ -17,74 +17,86 @@ class NuclearTest extends \PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $this->object = new Nuclear;
+        $this->nuclear = new Nuclear;
     }
-
     /**
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
      */
     protected function tearDown()
     {
-    }
 
+    }
     /**
      * @covers Monmiel\MonmielApiModelBundle\Model\Parc\Nuclear::setPowerNuclear
-     * @todo   Implement testSetPowerNuclear().
      */
     public function testSetPowerNuclear()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $PowerNuclear = 500;
+        $percentOfMix = 0.50;
+        $this->nuclear->setPowerNuclear($PowerNuclear,$percentOfMix);
+        $this->assertNotNull($this->nuclear->getFacteurChargeNuclear(), "this value mast not null becase it is initialize at setPowerNuclear method ");
+
+        //cas 1, pourcentage mix >= 0.75
+        $percentOfMix = 0.88;
+        $exeptedValue = 0.76;//max
+        $this->nuclear->setPowerNuclear($PowerNuclear,$percentOfMix);
+        $result = $this->nuclear->getFacteurChargeNuclear();
+        assertThat($result, is($exeptedValue));
+
+        //cas 2: 0.25<pourcentage mix < 0.75
+        $percentOfMix = 0.50;
+        $exeptedValue = 0.855;//(0.75 - 0.50)*0.38 + 0.75
+        $this->nuclear->setPowerNuclear($PowerNuclear,$percentOfMix);
+        $result = $this->nuclear->getFacteurChargeNuclear();
+        assertThat($result, is($exeptedValue));
+
+        //cas 3: pourcentage mix <= 0.25
+        $percentOfMix = 0.25;
+        $exeptedValue = 0.95;//max
+        $this->nuclear->setPowerNuclear($PowerNuclear,$percentOfMix);
+        $result = $this->nuclear->getFacteurChargeNuclear();
+        assertThat($result, is($exeptedValue));
     }
 
     /**
      * @covers Monmiel\MonmielApiModelBundle\Model\Parc\Nuclear::getPowerNuclear
-     * @todo   Implement testGetPowerNuclear().
      */
     public function testGetPowerNuclear()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $PowerNuclear = 500;
+        $percentOfMix = 0.50;
+        $this->nuclear->setPowerNuclear($PowerNuclear,$percentOfMix);
+        $this->assertEquals(0.855,   $this->nuclear->getFacteurChargeNuclear());
+        $exeptedValue = 500*1000/855;
+        $result = $this->nuclear->getPowerNuclear();
+        assertThat($result, is($exeptedValue));
+
     }
 
-    /**
-     * @covers Monmiel\MonmielApiModelBundle\Model\Parc\Nuclear::getFacteurChargeNuclear
-     * @todo   Implement testGetFacteurChargeNuclear().
-     */
-    public function testGetFacteurChargeNuclear()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers Monmiel\MonmielApiModelBundle\Model\Parc\Nuclear::getTauxDisponibiliteNuclear
-     * @todo   Implement testGetTauxDisponibiliteNuclear().
-     */
-    public function testGetTauxDisponibiliteNuclear()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+    public function testConstructeur(){
+        $this->nuclear = new Nuclear();
+        //they must take the default values
+        $this->assertNotNull($this->nuclear->getFacteurChargeNuclear(),"this value must a default");
+        $this->assertNotNull($this->nuclear->getParcNuclear(),"this value must a default");
+        $this->assertNotNull($this->nuclear->getPowerNuclear(),"this value must a default");
     }
 
     /**
      * @covers Monmiel\MonmielApiModelBundle\Model\Parc\Nuclear::getParcNuclear
-     * @todo   Implement testGetParcNuclear().
      */
     public function testGetParcNuclear()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $PowerNuclear = 500;
+        $percentOfMix = 0.50;
+        $exeptedValue = 0.855;//(0.75 - 0.50)*0.38 + 0.75
+        //facteur de charge
+        $this->nuclear->setPowerNuclear($PowerNuclear,$percentOfMix);
+        $result = $this->nuclear->getFacteurChargeNuclear();
+        assertThat($result, is($exeptedValue));
+        //à faire après confirmation Talbot
+
     }
+
+
 }
