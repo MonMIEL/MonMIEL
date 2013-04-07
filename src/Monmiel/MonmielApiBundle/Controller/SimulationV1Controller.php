16dcfa16917bb339832befb209b84f0adc128c1c
Paul Valla <paul.valla@gmail.com>
2013-04-07 18:08:17 +0200

    Clean Controller

--- src/Monmiel/MonmielApiBundle/Controller/SimulationV1Controller.php
+++ src/Monmiel/MonmielApiBundle/Controller/SimulationV1Controller.php
@@ -3,13 +3,13 @@
 namespace Monmiel\MonmielApiBundle\Controller;
 
 use JMS\DiExtraBundle\Annotation as DI;
+use Monmiel\MonmielApiModelBundle\Model\Mesure;
+use Monmiel\MonmielApiModelBundle\Model\Response\SimulationResultSeries;
+use Monmiel\MonmielApiModelBundle\Model\Year;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request as HttpRequest;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Monmiel\MonmielApiModelBundle\Model\Mesure;
-use Monmiel\MonmielApiModelBundle\Model\Year;
-use Monmiel\MonmielApiModelBundle\Model\Response\SimulationResultSeries;
 /**
  * @DI\Service("monmiel.simulation.controller")
  */
@@ -22,7 +22,6 @@ class SimulationV1Controller extends Controller
      */
     public function sendSimulationResult(HttpRequest $request)
     {
-        $response = new Response();
         $this->init($request);
 
         $result = new SimulationResultSeries();
@@ -52,27 +51,43 @@ class SimulationV1Controller extends Controller
     }
 
     /**
+     * Initialisation de la simulation
      * @param \Symfony\Component\HttpFoundation\Request $request
      */
     public function init(HttpRequest $request = null)
     {
         $this->stopWatch->start("init", "controller");
-        $userConsoMesure = new Mesure($request->get("targetConso"), \Monmiel\Utils\ConstantUtils::TERAWATT_HOUR);
+        /**
+         * On renseigne le service transformers, des la consommation total en 2011 (531 TwH)
+         * et celle qu'a rentré l'utilisateur.
+         */
         $actualConsoMesure = new Mesure(531, \Monmiel\Utils\ConstantUtils::TERAWATT_HOUR);
-
+        $userConsoMesure = new Mesure($request->get("targetConso"), \Monmiel\Utils\ConstantUtils::TERAWATT_HOUR);
         $this->transformers->setConsoTotalActuel($actualConsoMesure);
         $this->transformers->setConsoTotalDefinedByUser($userConsoMesure);
 
+        /**
+         * On crée l'objet Year de référence, seulement 2011 actuellement.
+         * On crée l'objet Year que l'utilisateur souhaite atteindre, tout cela à partir de la request.
+         * Et on renseigne le tout au service transformers.
+         */
         $refYear = $this->createRefYearObject();
         $targetYear = $this->createTargetYearObject($request);
-
         $this->transformers->setReferenceYear($refYear);
         $this->transformers->setTargetYear($targetYear);
 
-        $this->parc->setTargetParcPower($targetYear);
+        /**
+         * On demande au service de Parc de calculer le parc de référence et celui qu'on vise à partir de nos
+         * deux objets Year.
+         */
         $this->parc->setRefParcPower($refYear);
+        $this->parc->setTargetParcPower($targetYear);
         $targetParcPower = $this->parc->getTargetParcPower();
         $refParcPower = $this->parc->getRefParcPower();
+
+        /**
+         * On renseigne au service repartition les parcs calculés et les objets Years.
+         */
         $this->repartition->setReferenceYear($refYear);
         $this->repartition->setTargetYear($targetYear);
         $this->repartition->setReferenceParcPower($refParcPower);
@@ -93,60 +108,6 @@ class SimulationV1Controller extends Controller
      {
          return new Year(2011, 419801949, 11253649, 2000000, 0, 38000000, 0);
      }
-
-    public function getContent()
-    {
-        $content = <<<EOF
-{"simulation" :
-   { "present-data":
-     {
-         "nucleaire": 50,
-         "Photovoltaique" : 10,
-         "Eolien" : 5,
-         "Hydraulique" : 5,
-         "Centrales flammes": 30,
-         "STEP" : 0
-     },
- "target-data":
-     [
-         {"name" : "nucleaire", "y": 35, "z": -15},
-         {"name" : "Photovoltaique", "y": 15, "z":5},
-{"name" : "Eolien","y": 15, "z":10},
-{"name" : "Hydraulique","y": 5,"z":0},
-{"name" : "Centrales a flammes","y": 30,"z":0},
-{"name" : "STEP","y": 0,"z":0}
-     ],
-"series": [
-       {
-         "name": "Nucleaire",
-         "data": [ 502, 635, 809, 947, 1402, 3634, 5268, 502, 635, 809, 947, 1402, 3634, 5268]
-      },
-      {  "name": "Photovoltaique",
-         "data": [106, 107, 111, 133, 221, 767, 1766]
-      },
-      {  "name": "Eolien",
-         "data": [163, 203, 276, 408, 547, 729, 628]
-      },
-      {  "name": "Hydraulique",
-         "data": [18, 31, 54, 156, 339, 818, 1201]
-      },
-      {  "name": "Centrales flammes",
-         "data": [2, 2, 2, 6, 13, 30, 46]
-      },
-      {  "name": "STEP",
-         "data": [0, 0, 0, 0, 0, 0, 0]
-      },
-      {  "name": "Import",
-         "data": [0, 0, 0, 0, 0, 0, 0]
-      }
-      ]
-   }
-
-}
-EOF;
-        return $content;
-
-    }
 
     /**
      * @var \Monmiel\MonmielApiBundle\Services\TransformersService\TransformersV1
